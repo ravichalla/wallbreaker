8a43ed7432288b3fa9286ae4941e77182881c088
venkata ravi <ravichalla95@gmail.com>
2019-07-06 22:42:50 -0500

    files added

--- week2/design_hashmap.py
+++ week2/design_hashmap.py
@@ -1,56 +1,40 @@
-class MyHashMap(object):
-
+class MyHashMap:
     def __init__(self):
-        self.size = 6
-        self.map = [None] * self.size
-
-    def _get_hash(self, key):
-        return key % self.size
+        self.cap = 5000
+        self.arr = [None] * self.cap
 
     def put(self, key, value):
-        key_hash = self._get_hash(key)
-        key_value = [key, value]
-        if self.map[key_hash] is None:
-            self.map[key_hash] = list([key_value])
-            #print(self.map)
-            return True
+        hash_val = hash(key) % self.cap
+        if self.arr[hash_val] == None:
+            self.arr[hash_val] = [(key, value)]
         else:
-            for pair in self.map[key_hash]:
-                if pair[0] == key:
-                    pair[1] = value
-                    return True
-            self.map[key_hash].append(key_value)
-            #print(self.map)
-            return True
+            for ind in range(len(self.arr[hash_val])):
+                tup = self.arr[hash_val][ind]
+                if tup[0] == key:
+                    self.arr[hash_val][ind] = (key, value)
+                    return
+            self.arr[hash_val].append((key, value))
 
-    def get(self,key):
-        key_hash = self._get_hash(key)
-        if self.map[key_hash] is not None:
-            for pair in self.map[key_hash]:
-                if pair[0]== key:
-                    return pair[1]
+    def get(self, key):
+        hash_val = hash(key) % self.cap
+        if self.arr[hash_val] != None:
+            for tup in self.arr[hash_val]:
+                if tup[0] == key:
+                    return tup[1]
         return -1
 
-    def remove(self,key):
-        key_hash = self._get_hash(key)
-        if self.map[key_hash] is None:
-            return False
-        for i in range(0,len(self.map[key_hash])):
-            if self.map[key_hash][i][0] == key:
-                self.map[key_hash].pop(i)
-            #print(self.map)
-            return True
+    def remove(self, key):
+        hash_val = hash(key) % self.cap
+        if self.arr[hash_val] != None:
+            for ind in range(len(self.arr[hash_val])):
+                if self.arr[hash_val][ind][0] == key:
+                    del self.arr[hash_val][ind]
+                    return
 
-hashMap = MyHashMap()
-hashMap.put(1, 1);
-hashMap.put(2, 2);
-print(hashMap.get(1))         # returns 1
-print(hashMap.get(3))            # returns -1 (not found)
-hashMap.put(2, 1);          # update the existing value
-print(hashMap.get(2))          # returns 1
-hashMap.remove(2);          # remove the mapping for 2
-print(hashMap.get(2))           # returns -1 (not found)
+'''
+Idea/thoughts:
+implement the put, get , remove mehtods,
+used a hash and list of size 5000, it can be any no.
 
-
-["MyHashMap","remove","put","remove","remove","get","remove","put","get","remove","put","put","put","put","put","put","put","put","put","put","put","remove","put","put","get","put","get","put","put","get","put","remove","remove","put","put","get","remove","put","put","put","get","put","put","remove","put","remove","remove","remove","put","remove","get","put","put","put","put","remove","put","get","put","put","get","put","remove","get","get","remove","put","put","put","put","put","put","get","get","remove","put","put","put","put","get","remove","put","put","put","put","put","put","put","put","put","put","remove","remove","get","remove","put","put","remove","get","put","put"]
-[[],[27],[65,65],[19],[0],[18],[3],[42,0],[19],[42],[17,90],[31,76],[48,71],[5,50],[7,68],[73,74],[85,18],[74,95],[84,82],[59,29],[71,71],[42],[51,40],[33,76],[17],[89,95],[95],[30,31],[37,99],[51],[95,35],[65],[81],[61,46],[50,33],[59],[5],[75,89],[80,17],[35,94],[80],[19,68],[13,17],[70],[28,35],[99],[37],[13],[90,83],[41],[50],[29,98],[54,72],[6,8],[51,88],[13],[8,22],[85],[31,22],[60,9],[96],[6,35],[54],[15],[28],[51],[80,69],[58,92],[13,12],[91,56],[83,52],[8,48],[62],[54],[25],[36,4],[67,68],[83,36],[47,58],[82],[36],[30,85],[33,87],[42,18],[68,83],[50,53],[32,78],[48,90],[97,95],[13,8],[15,7],[5],[42],[20],[65],[57,9],[2,41],[6],[33],[16,44],[95,30]]
+put each ele in hash position.
+'''
