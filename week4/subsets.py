eb121ffb79e1405b713784ca86e812ea30861919
Ravi Challa <ravichalla95@gmail.com>
2019-08-03 06:57:18 -0500

    files added

---
+++ week4/subsets.py
@@ -0,0 +1,43 @@
+'''
+QUESTION:
+78. Subsets
+
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+'''
+
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = [[]]
+        for num in sorted(nums):
+            res += [item+[num] for item in res ]
+            print(res)
+        return res
+    
+'''
+Ideas/thoughts:
+iterating through the nums, join previous res ele and each num.
+return ans
+'''
